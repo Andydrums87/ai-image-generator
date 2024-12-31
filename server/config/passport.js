4a42d0d650a35d9717420d05eb868826805cb364
Andy Joseph <neon_4@hotmail.com>
2024-12-31 18:00:12 +0000

    update scope

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: process.env.GOOGLE_CALLBACK,
-            scope: ['profile', 'email']
+            // scope: ['profile', 'email']
         },
         async (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }).then(existingUser => {
