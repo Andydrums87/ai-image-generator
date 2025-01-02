edc0a4eaa183d11e03dcc0ca6c0b11cc6cb030a4
Andy Joseph <neon_4@hotmail.com>
2025-01-02 11:35:31 +0000

    uopdate facebook client info

--- server/config/passport.js
+++ server/config/passport.js
@@ -34,8 +34,8 @@ passport.use(
         passport.use(
             new FacebookStrategy(
                 {
-                    clientID: process.env.FACEBOOK_CLIENT_ID,
-                    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+                    clientID: process.env.FACEBOOK_APP_ID,
+                    clientSecret: process.env.FACEBOOK_APP_SECRET,
                     callbackURL: process.env.FACEBOOK_CALLBACK,
                     profileFields: ['id', 'displayName', 'photos', 'emails', 'name']
                     // passReqToCallback: true,
